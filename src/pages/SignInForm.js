c8e16b093d7d125f173147010334d31f8a5c8473
Phoong Shen Yang <shenyang_zx@hotmail.com>
2020-03-10 19:06:42 +0800

    moving first page onto component
    And changing oldschool react to newschool. WIP

--- src/pages/SignInForm.js
+++ src/pages/SignInForm.js
@@ -1,57 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-class SignInForm extends Component {
-    constructor() {
-        super();
+const SignInForm = () => {
+  const [emailText, setEmailText] = useState('')
+  const [passwordText, setPasswordText] = useState('')
 
-        this.state = {
-            email: '',
-            password: ''
-        };
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+  const handleChange = (e) => {
+    if (e.target.id == 'email') {
+      setEmailText(e.target.value)
     }
-
-    handleChange(e) {
-        let target = e.target;
-        let value = target.type === 'checkbox' ? target.checked : target.value;
-        let name = target.name;
-
-        this.setState({
-          [name]: value
-        });
+    else {
+      setPasswordText(e.target.value)
     }
+    console.log(emailText)
+    console.log(passwordText)
+  }
 
-    handleSubmit(e) {
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
 
-        console.log('The form was submitted with the following data:');
-        console.log(this.state);
-    }
 
-    render() {
-        return (
-        <div className="FormCenter">
-            <form onSubmit={this.handleSubmit} className="FormFields" onSubmit={this.handleSubmit}>
-            <div className="FormField">
-                <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
-                <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" value={this.state.email} onChange={this.handleChange} />
-              </div>
+  return (
+    <div className="FormCenter">
+      <form onSubmit={handleSubmit} className="FormFields">
+        <div className="FormField">
+          <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
+          <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" value={emailText} onChange={handleChange} />
+        </div>
 
-              <div className="FormField">
-                <label className="FormField__Label" htmlFor="password">Password</label>
-                <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password" value={this.state.password} onChange={this.handleChange} />
-              </div>
+        <div className="FormField">
+          <label className="FormField__Label" htmlFor="password">Password</label>
+          <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password" value={passwordText} onChange={handleChange} />
+        </div>
 
-              <div className="FormField">
-                  <button className="FormField__Button mr-20">Sign In</button> <Link to="/" className="FormField__Link">Create an account</Link>
-              </div>
-            </form>
-          </div>
-        );
-    }
+        <div className="FormField">
+          <button className="FormField__Button mr-20">Sign In</button> <Link to="/" className="FormField__Link">Create an account</Link>
+        </div>
+      </form>
+    </div>
+  );
 }
+
 
 export default SignInForm;
