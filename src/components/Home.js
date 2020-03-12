cfbd1976c15d3fddecdb256ceb4ebe99886810c3
ericltwee <ericltwee@gmail.com>
2020-03-12 10:29:28 +0800

    2ndbase

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Home.css";
+import Carousel from "react-bootstrap/Carousel";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import bloodtest from "./blood-tests.png";
 
-const Home = () => {
+const ControlledCarousel = () => {
+  const [index, setIndex] = useState(0);
+  const [direction, setDirection] = useState(null);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+    setDirection(e.direction);
+  };
+
   return (
-    <>
-      <p>nvabar</p>
-      <a>link1</a>
-      <a>link1</a>
-      <a>link1</a>
-      <a>link1</a>
-    </>
+    <div className="Menu">
+      <Carousel
+        activeIndex={index}
+        direction={direction}
+        onSelect={handleSelect}
+      >
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="./img/blood event.png"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src="event" alt="Second slide" />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src="info" alt="Third slide" />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
   );
 };
 
-export default Home;
+export default ControlledCarousel;
