eab2696591b4f5bcefc2d2adc3e24acd605ff0c5
Phoong Shen Yang <shenyang_zx@hotmail.com>
2020-04-03 14:24:15 +0800

    previous commit was faulty

---
+++ src/pages/Events.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+
+const Events = () => {
+    const [events, setEvents] = useState([])
+    useEffect(() => {
+        axios({
+            method: "GET",
+            url: "https://team-osm.herokuapp.com/api/v1/events/",
+        })
+            .then(response => {
+                console.log(response);
+                setEvents(response.data)
+            })
+
+            .catch(error => {
+                console.error(error);
+            });
+    }, []);
+
+    return (
+        <div>
+            Please halp
+            {events.map(event => (
+                <div>
+                    =======
+                    <br>
+                    </br>
+                    {event.date}
+                    <p>
+                        {event.location}
+                    </p>
+                    <p>
+                        {event.description}
+                    </p>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Events
