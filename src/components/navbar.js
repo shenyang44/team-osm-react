21bac14f27e6a9357155cec4e78dee6796db6087
ericltwee <ericltwee@gmail.com>
2020-05-05 17:08:14 +0800

    navbar coloring

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,28 +1,45 @@
 import React from "react";
-import { Navbar, Nav, NavbarBrand, NavLink } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
+import "./navbar.css";
 
 const NAVBAR = (props) => {
   return (
-    <Navbar collapseOnSelect expand="md">
-      <Navbar.Brand href="/">Bloodi</Navbar.Brand>
+    <Navbar className="Nav" collapseOnSelect expand="md">
+      <Navbar.Brand className="NavLink" href="/">
+        Bloodi
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="/blood">About Blood</Nav.Link>
-          <Nav.Link href="/donate">FAQ</Nav.Link>
-          <Nav.Link href="/events">Events</Nav.Link>
-          <Nav.Link href="/map">Locate</Nav.Link>
-          <Nav.Link href="/profile">Profile</Nav.Link>
+          <Nav.Link className="NavLink" href="/blood">
+            About Blood
+          </Nav.Link>
+          <Nav.Link className="NavLink" href="/donate">
+            FAQ
+          </Nav.Link>
+          <Nav.Link className="NavLink" href="/events">
+            Events
+          </Nav.Link>
+          <Nav.Link className="NavLink" href="/map">
+            Locate
+          </Nav.Link>
+          <Nav.Link className="NavLink" href="/profile">
+            Profile
+          </Nav.Link>
         </Nav>
         <Nav>
           {props.loggedIn ? (
-            <Nav.Link href="" onClick={props.logout}>
+            <Nav.Link className="NavLink" href="" onClick={props.logout}>
               Logout
             </Nav.Link>
           ) : (
-            <Nav.Link href="/login">Get Started</Nav.Link>
+            <Nav.Link className="NavLink" href="/login">
+              Get Started
+            </Nav.Link>
           )}
-          <Nav.Link href="/contact">Contact Us</Nav.Link>
+          <Nav.Link className="NavLink" href="/contact">
+            Contact Us
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
