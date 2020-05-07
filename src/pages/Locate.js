f74a9d02e00afe9d4159a8be6ab41b890a62f72a
ericltwee <ericltwee@gmail.com>
2020-05-07 22:25:05 +0800

    googleMap test #1

--- src/pages/Locate.js
+++ src/pages/Locate.js
@@ -1,26 +1,50 @@
 import React from "react";
 import "./Locate.css";
 import Maps from "../components/maptab";
+import styled from "styled-components";
 
 const Locate = () => {
+  const GoogleMap = styled.iframe`
+    border: 1px solid;
+  `;
+
   return (
     <div className="mapBox">
       <Maps>
         <div label="Clinic">
-          See ya later, <em>Alligator</em>!
+          <GoogleMap
+            width="600"
+            height="450"
+            // frameborder="0"
+            // style="border:0;"
+            // allowfullscreen=""
+            // aria-hidden="false"
+            tabindex="0"
+            src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d63741.269887259645!2d101.60830061280357!3d3.1397194294271746!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sblood%20donation%20center!5e0!3m2!1sen!2smy!4v1588841876972!5m2!1sen!2smy"
+          ></GoogleMap>
         </div>
-        <div label="Hospital">
-          After &apos;while, <em>Crocodile</em>!
-        </div>
-        <div label="BloodBank">
-          Nothing to see here, this tab is <em>extinct</em>!
+        <div label="Hospital">After</div>
+        <div label="Blood Donation Centres">
+          Nothing
+          {/* <GoogleMap src="https://goo.gl/maps/VmBt5LejUCHMpNKD8"></GoogleMap>
+          <GoogleMap src="https://goo.gl/maps/VmBt5LejUCHMpNKD8"></GoogleMap> */}
         </div>
       </Maps>
     </div>
   );
 };
 
-// const container = document.createElement("div");
-// document.body.appendChild(container);
-// render(<Locate />, container);
 export default Locate;
+
+{
+  /* <iframe
+  src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d63741.269887259645!2d101.60830061280357!3d3.1397194294271746!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sblood%20donation%20center!5e0!3m2!1sen!2smy!4v1588841876972!5m2!1sen!2smy"
+  width="600"
+  height="450"
+  frameborder="0"
+  style="border:0;"
+  allowfullscreen=""
+  aria-hidden="false"
+  tabindex="0"
+></iframe>; */
+}
